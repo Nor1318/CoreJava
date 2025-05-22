adbc66e321e111a2d9ccfe4a6bdd60d39ef5be94
Bobby Dahal <IAMBOBBYDAHAL@GMAIL.COM>
2025-05-22 06:22:12 +0000

    Variables in Java

--- 01_basics/Hello.java
+++ 01_basics/Hello.java
@@ -1,6 +1,10 @@
 
 class Hello{
     public static void main(String a[]){
-        System.out.println("Hello, World!");
+        int num1 = 5;  // Variable Decleration
+        int num2 = 6;
+        int result = num1 + num2;
+        System.out.println(result);  // Standard printing stmt ln means print on new line
+        
     }
 }
