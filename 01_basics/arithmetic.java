a8142ebda30a0726bd8c828a02639b77b33dcd8d
Bobby Dahal <IAMBOBBYDAHAL@GMAIL.COM>
2025-05-23 10:38:03 +0000

    Arithmetic operators in Java

---
+++ 01_basics/arithmetic.java
@@ -0,0 +1,24 @@
+class arithmetic{
+    public static void main(String args[]){
+        int num1 = 9;
+        int num2 = 6;
+        // int result = num1 + num2;
+        // int result = num1 - num2;
+        // int result = num1 * num2;
+        // int result = num1 / num2;
+        // int result = num1 % num2;
+        // System.out.println(result);
+        // num1 = num1 + 2;
+        // num1 += 2;
+        // num1 += 1;
+        // num1++; // post increment
+        // ++num1; // pre increment
+        System.out.println(num1);
+        // int result = num1++; //fetch then increment = 9
+        // int result = ++num1; //increment then fetch = 10
+
+        
+        System.out.println(result);
+
+    }
+}
