fb6bf5e98143e25c22a5e3b55b9d5d557b5baad5
Bobby Dahal <IAMBOBBYDAHAL@GMAIL.COM>
2025-05-23 13:07:27 +0000

    Basic of relational operator in java

---
+++ 01_basics/RelationalOperator.java
@@ -0,0 +1,12 @@
+class RelationalOperator{
+    public static void main(String args[]){
+        double x = 10.5;
+        double y = 10.9;
+
+        int z = 5;
+        int a = 9;
+
+        boolean result = x <= y;
+        System.out.println(result);
+    }
+}
